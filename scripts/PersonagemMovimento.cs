b46e018ed5fd7bd5ff751db32654c5caaaa24f69
Gabriel Cardoso Nunes <165850475+GabrielCardoso10@users.noreply.github.com>
2024-10-17 22:44:50 -0300

    Update PersonagemMovimento.cs

--- scripts/PersonagemMovimento.cs
+++ scripts/PersonagemMovimento.cs
@@ -6,29 +6,23 @@ public class PersonagemMovimento : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    // Método Awake é chamado antes do Start
     private void Awake()
     {
-        // Obtém a referência ao Rigidbody2D anexado ao GameObject
         rb = GetComponent<Rigidbody2D>();
 
-        // Verifica se o Rigidbody2D foi encontrado, caso contrário, exibe uma mensagem de erro
         if (rb == null)
         {
             Debug.LogError("Rigidbody2D não encontrado! Adicione um Rigidbody2D ao GameObject.");
         }
     }
    
-    // Método Update é chamado uma vez por quadro
+
     void Update()
     {
-        // Verifica se o Rigidbody2D está presente e se o botão do mouse foi pressionado
         if (rb != null && Input.GetMouseButtonDown(0))
         {
-            // Define a velocidade do Rigidbody2D para zero
             rb.velocity = Vector2.zero;
 
-            // Aplica uma força para mover o personagem para cima
             rb.AddForce(Vector2.up * 150);
         }
     }
